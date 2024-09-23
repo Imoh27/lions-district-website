56e2fc61f28192295ef7dccdd2301351caf69c53
SirBen <akawuben@gmail.com>
2024-09-24 00:29:39 +0100

    Colors update

--- plugins/google-map/map.js
+++ plugins/google-map/map.js
@@ -94,7 +94,7 @@ function initialize() {
       "featureType": "road",
       "elementType": "geometry",
       "stylers": [{
-          "hue": "#ffb600"
+          "hue": "#0a3dab"
         },
         {
           "saturation": "100"
@@ -111,7 +111,7 @@ function initialize() {
       "featureType": "road",
       "elementType": "geometry.stroke",
       "stylers": [{
-          "color": "#ffb600"
+          "color": "#0a3dab"
         },
         {
           "saturation": "0"
